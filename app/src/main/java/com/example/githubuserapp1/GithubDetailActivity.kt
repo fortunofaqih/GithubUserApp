33fc3777111a54fcd5649c28d2cdd7375185ee6b
fortunofaqih <87749389+fortunofaqih@users.noreply.github.com>
2022-08-04 05:29:24 +0700

    Update GithubDetailActivity.kt
    penambahan data received

--- app/src/main/java/com/example/githubuserapp1/GithubDetailActivity.kt
+++ app/src/main/java/com/example/githubuserapp1/GithubDetailActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import android.util.Log
 
 class GithubDetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_GITHUB = "extra_github"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_github_detail)
-        val data = intent.getParcelableExtra<Github>("DATA")
+        //val data = intent.getParcelableExtra<Github>("DATA")
+        
+         val tvDataReceived: TextView = findViewById(R.id.tv_item_username)
+        val data = intent.getParcelableExtra<Github>(EXTRA_GITHUB) as Github
+        val text = "Username : ${data.username.toString()},\nname : ${data.name},\nLocation : ${data.location},\nRepository : ${data.repository}"
+        tvDataReceived.text = text
 
     }
 }
